7beb6b49547ce1c5ec4b3748f68a80951ff0bce3
Roy <63487435+RodgerLugo@users.noreply.github.com>
2025-05-22 00:23:29 -0700

    Update front.js

--- js/front.js
+++ js/front.js
@@ -174,67 +174,7 @@ $(function () {
 
 
 
-    /* =========================================
-     *  Hobby
-     *  =======================================*/
 
-
-    $('.hobby-reference a').on('click', function (e) {
-
-        e.preventDefault();
-
-        var title = $(this).find('.reference-title').text(),
-            description = $(this).siblings('.reference-description').html();
-
-        $('#hobby-detail-title').text(title);
-        $('#hobby-detail-content').html(description);
-
-        var images = $(this).siblings('.reference-description').data('images').split(',');
-        if (images.length > 0) {
-            sliderContent = '';
-            for (var i = 0; i < images.length; ++i) {
-                sliderContent = sliderContent + '<div class="item"><img src=' + images[i] + ' alt="" class="img-fluid"></div>';
-            }
-        } else {
-            sliderContent = '';
-        }
-
-        openReference(sliderContent);
-
-    });
-
-    function openReference(sliderContent) {
-        $('#hobby-detail').slideDown();
-        $('#hobby-masonry').slideUp();
-
-
-        if (sliderContent !== '') {
-
-            var slider = $('#hobby-detail-slider');
-
-            if (slider.hasClass('owl-loaded')) {
-                slider.trigger('replace.owl.carousel', sliderContent);
-            } else {
-                slider.html(sliderContent);
-                slider.owlCarousel({
-                    nav: false,
-                    dots: true,
-                    items: 1
-                });
-
-            }
-        }
-    }
-
-
-    function closeReference() {
-        $('#hobby-masonry').slideDown();
-        $('#hobby-detail').slideUp();
-    }
-
-    $('#filter button, #detail .close').on('click', function () {
-        closeReference();
-    });
 
 
 
